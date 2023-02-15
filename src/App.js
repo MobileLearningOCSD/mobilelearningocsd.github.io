9562baaf8a284f48a59938b036a65c1f8e9fe534
William Hagan <williamhagan@iMacBlue.local>
2023-02-15 13:48:36 -0600

    using process.env to hopefully fix routing error in deployed project

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
       <main>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Layout />}>
+            <Route path={process.env.PUBLIC_URL + '/'} element={<Layout />}>
               <Route index element={ <Home /> } /> 
-              <Route path="limit-ip-address-tracking" element={ <LimitIPAddressTracking /> } />
+              <Route path={process.env.PUBLIC_URL + '/limit-ip-address-tracking'} element={ <LimitIPAddressTracking /> } />
               <Route path="test" element={ <Test /> } />
               <Route path="*" element={<FourOFour />} />
             </Route>
